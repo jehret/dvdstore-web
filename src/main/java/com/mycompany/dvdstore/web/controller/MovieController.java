db99a30038305cd6ec718bd721da9d2c3a2b325d
jp <jp.ehret@laposte.net>
2020-05-14 18:35:21 +1200

    Détail d'un film

---
+++ src/main/java/com/mycompany/dvdstore/web/controller/MovieController.java
@@ -0,0 +1,34 @@
+package com.mycompany.dvdstore.web.controller;
+
+import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.service.MovieServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Scanner;
+
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+    @Autowired
+    private MovieServiceInterface movieService;
+
+    public MovieServiceInterface getMovieService() {
+        return movieService;
+    }
+
+    public void setMovieService(MovieServiceInterface movieService) {
+        this.movieService = movieService;
+    }
+
+    @RequestMapping("/{id}")
+    public String displayMovieCard(@PathVariable("id") long id, Model model){
+        model.addAttribute("movie",movieService.getMovieById(id));
+        return "movie-details";
+    }
+
+}
