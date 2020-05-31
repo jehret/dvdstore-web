b9ceef370297755e31768d5d305d15118d8a7abc
jp <jp.ehret@laposte.net>
2020-05-31 13:44:33 +1200

    Restcontroller

--- src/main/java/com/mycompany/dvdstore/web/api/MovieController.java
+++ src/main/java/com/mycompany/dvdstore/web/api/MovieController.java
@@ -1,4 +1,4 @@
-package com.mycompany.dvdstore.web.controller;
+package com.mycompany.dvdstore.web.api;
 
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.service.MovieServiceInterface;
@@ -10,9 +10,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Scanner;
 
-@Controller
+@RestController
 @RequestMapping("/movie")
 public class MovieController {
 
@@ -27,25 +28,19 @@ public class MovieController {
         this.movieService = movieService;
     }
 
+    @GetMapping
+    public List<Movie> list(){
+        return movieService.getMovieList();
+    }
+
     @GetMapping("/{id}")
-    public String displayMovieCard(@PathVariable("id") long id, Model model){
-        model.addAttribute("movie",movieService.getMovieById(id));
-        return "movie-details";
+    public Movie get(@PathVariable("id") long id){
+        return movieService.getMovieById(id);
     }
 
     @PostMapping
-    public String addMovie(@Valid @ModelAttribute MovieForm form, BindingResult results){
-
-        if (results.hasErrors()){
-            return "add-movie-form";
-        }
-        Movie movie = new Movie();
-        movie.setTitle(form.getTitle());
-        movie.setGenre(form.getGenre());
-        movie.setDescription(form.getDescription());
-
-        movieService.registerMovie(movie);
-        return "movie-added";
+    public Movie addMovie(@RequestBody Movie movie){
+        return movieService.registerMovie(movie);
     }
 
 
