db370f6f6a4dd6ad32d9fb864c20392ef6f3583b
Jean-Philippe Ehret <jeanphiehret@gmail.com>
2025-01-07 11:45:38 +1300

    35 not 36

--- src/main/java/com/mycompany/dvdstore/web/form/MovieForm.java
+++ src/main/java/com/mycompany/dvdstore/web/form/MovieForm.java
@@ -13,11 +13,6 @@ public class MovieForm {
     private String genre;
     @Size(max = 255, message = "Entrez une description de 255 caractères au plus")
     private String description;
-    @NotBlank(message = "Veuillez entrer un prénom")
-    @Size(max = 20, message = "Entrez un prénom de 20 caractères au plus")
-    private String firstName;
-    @Size(max = 20, message = "Entrez un nom de 20 caractères au plus")
-    private String lastName;
 
     public String getTitle() {
         return title;
